34afd787d8ddcc403b4c2e9fe61ebdd202b0ddaf
Sergio Rojas <sergio.rojas.cch@gmail.com>
2021-05-22 18:49:53 -0600

    fixes

--- sketch.js
+++ sketch.js
@@ -36,37 +36,38 @@ function draw()
 
 function ecuPP(x1, y1, x2, y2)
 {
- 	let x = x1,
-    y = y1,
-    stepX = 1,
-    stepY = 1;
-  	const dx = x2 - x1;
-  	const dy = y2 - y1;
+	let x = x1,
+	y = y1,
+	stepX = 1,
+	stepY = 1;
+	const dx = x2 - x1;
+	const dy = y2 - y1;
 
-  	if (dx == 0) 
-  	{
-    	if (dy < 0) stepY = -1;
-    	
-    	while (y !== y2) 
-    	{
-      		point(x, y);
-      		y += stepY;
-    	}
-  } 
-  else 
-  {
+	if (dx == 0) 
+	{
+	
+		if (dy < 0) stepY = -1;
+	
+		while (y !== y2) 
+		{
+			point(x, y);
+			y += stepY;
+		}
+	} 
+	else 
+	{
 		const m = dy / dx;
-    	const b = y1 - m * x1;
-    	
-    	if (dx < 0) stepX = -1;
-    
-	    while (x !== x2) 
-	    {
-	      	point(x, y);
-	      	x += stepX;
-	      	y = m * x + b;
-	    }
-  }
+		const b = y1 - m * x1;
+	
+		if (dx < 0) stepX = -1;
+	
+		while (x !== x2) 
+		{
+			point(x, y);
+			x += stepX;
+			y = m * x + b;
+		}
+	}
 }
 
 function dda(x1, y1, x2, y2)
@@ -75,17 +76,17 @@ function dda(x1, y1, x2, y2)
 	y = y1,
 	dx = x2 - x1,
 	dy = y2 - y1;
-
-  	const m = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
-  	const xIncrement = dx / m;
-  	const yIncrement = dy / m;
 	
-  	for (let i = 0; i < m; i++)
-  	{
-    	x += xIncrement;
-    	y += yIncrement;
-   		point(x, y);
-  	}
+	const m = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
+	const xIncrement = dx / m;
+	const yIncrement = dy / m;
+
+	for (let i = 0; i < m; i++)
+	{
+		x += xIncrement;
+		y += yIncrement;
+		point(x, y);
+	}
 }
 
 function bresenham (x1, y1, x2, y2)
@@ -108,45 +109,45 @@ function bresenham (x1, y1, x2, y2)
 		dx = -dx;
 		sx = -1;
 	}
-
+	
 	if (dx > dy) 
 	{
 		let p = 2 * dy - dx;
-		
+
 		while (x != x2) 
 		{
-	  		point(x, y);
-	  		x += sx;
-	  		
-	  		if (p < 0) 
-	  		{
-	    		p += 2 * dy;
-	  		} 
-		  	else 
-	  		{
-		    	y += sy;
-		    	p += 2 * (dy - dx);
-		  	}
+			point(x, y);
+			x += sx;
+
+			if (p < 0) 
+			{
+				p += 2 * dy;
+			} 
+			else 
+			{
+				y += sy;
+				p += 2 * (dy - dx);
+			}
 		}
 	} 
 	else 
 	{
 		let p = 2 * dx - dy;
-		
+
 		while (y != y2) 
 		{
-	  		point(x, y);
-	  		y += sy;
-	  		
-	  		if (p < 0) 
-	  		{
-		    	p += 2 * dx;
-		  	} 
-	  		else 
-	  		{
+			point(x, y);
+			y += sy;
+		
+			if (p < 0) 
+			{
+				p += 2 * dx;
+			} 
+			else 
+			{
 				x += sx;
 				p += 2 * (dx - dy);
-		  	}
+			}
 		}
 	}
-}
+}	
